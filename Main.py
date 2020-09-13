4adde552399f6c8b6b24459ac4554a24ae4cf1b1
Anonymoustank <pkadekodi@gmail.com>
2020-09-12 21:59:31 -0600

    Converted project to pygame

--- Main.py
+++ Main.py
@@ -1,35 +1,80 @@
-import pyglet
-from pyglet.window import key, mouse
-HEIGHT, WIDTH = 1280, 720
-window = pyglet.window.Window(HEIGHT, WIDTH, "Game", resizable = False)
-window.set_mouse_visible(False)
+import pygame as pg
+import random
+GREEN = (20, 255, 140)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+HEIGHT, WIDTH = 640, 720
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Snake")
 
-label = pyglet.text.Label('Press an arrow key to begin', font_name='Times New Roman', font_size=36, x=2 * (window.width//3), y=window.height//2, anchor_x='center', anchor_y='center')
+myfont = pg.font.SysFont('verdana', 25)
+
+textsurface = myfont.render("Press any arrow key to start", True, (WHITE))
 started = False
 has_won = False
 moving = False
 
-batch = pyglet.graphics.Batch()
-all_snakes = [pyglet.shapes.Rectangle(HEIGHT//2, WIDTH//2, 50, 50, color=(55, 55, 255), batch=batch)]
+class Block(pg.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        
+        self.image = pg.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
 
-all_locations = [all_snakes[0].position]
+        # Draw the player (a rectangle)
+        pg.draw.rect(self.image, color, [0, 0, width, height])
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.rect.center = (int(width/2), int(height/2))
 
-@window.event
-def on_draw():
-    window.clear()
+batch = pg.sprite.Group()
+all_snakes = [Block(GREEN, 50, 50)]
+all_locations = [(HEIGHT//2, WIDTH//2)]
+batch.add(all_snakes[0])
+apple = Block(RED, 15, 15)
+apple.rect.x = random.randint(100, WIDTH - 100)
+apple.rect.y = random.randint(100, HEIGHT - 100)
+
+apple_group = pg.sprite.Group()
+apple_group.add(apple)
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    screen.fill(BLACK)
     if started == False:
-        label.draw()
+        screen.blit(textsurface,(0,0))
     else:
-        batch.draw()
+        batch.update()
+        apple_group.update()
+        batch.draw(screen)
+        apple_group.draw(screen)
+    pg.display.update()
 
-def refresh(time):
-    global all_locations, all_snakes
+    keys = pg.key.get_pressed()
+    if has_won == False:
+        if keys[pg.K_a] or keys[pg.K_LEFT]:
+            moving = "Left"
+        elif keys[pg.K_d] or keys[pg.K_RIGHT]:
+            moving = "Right"
+        elif keys[pg.K_s] or keys[pg.K_DOWN]:
+            moving = "Down"
+        elif keys[pg.K_w] or keys[pg.K_UP]:
+            moving = "Up"
+    if moving != False:
+        started = True
+
     distance = 5
     new_x, new_y = all_locations[0]
     if moving == "Up":
-        new_y += distance
-    elif moving == "Down":
         new_y -= distance
+    elif moving == "Down":
+        new_y += distance
     elif moving == "Right":
         new_x += distance
     elif moving == "Left":
@@ -37,22 +82,8 @@ def refresh(time):
     del(all_locations[len(all_locations) - 1])
     all_locations = [(new_x, new_y)] + all_locations
     for i in range(len(all_locations)):
-        all_snakes[i].position = all_locations[i]
+        all_snakes[i].rect.x = all_locations[i][0]
+        all_snakes[i].rect.y = all_locations[i][1]
+pg.quit()
 
-@window.event
-def on_key_press(symbol, modifiers):
-    global started, moving
-    started = True
-    if has_won == False:
-        if symbol == key.A or symbol == key.LEFT:
-            moving = "Left"
-        elif symbol == key.D or symbol == key.RIGHT:
-            moving = "Right"
-        elif symbol == key.S or symbol == key.DOWN:
-            moving = "Down"
-        elif symbol == key.W or symbol == key.UP:
-            moving = "Up"
 
-if __name__ == "__main__":
-    pyglet.clock.schedule_interval(refresh, 1.0/120.0)
-    pyglet.app.run()
